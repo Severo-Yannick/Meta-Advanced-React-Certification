f5b60b38d066f14214948d4e37981b78a9d8d3d4
Yannick SEVERO <ysevero@squad.fr>
2024-09-06 18:23:41 +0200

    refacto: lab-3

---
+++ Module-1/lab-3-context-api/src/components/Header.js
@@ -0,0 +1,13 @@
+import Switch from "../Switch";
+import Title from "./Title";
+
+const Header = () => {
+  return (
+    <header>
+      <Title>Little Lemon 🍕</Title>
+      <Switch />
+    </header>
+  );
+};
+
+export default Header;
---
+++ Module-1/lab-3-context-api/src/components/Page.js
@@ -0,0 +1,13 @@
+import Content  from "./Content";
+import Title  from "./Title";
+
+const Page = () => {
+  return (
+    <div className="Page">
+      <Title>When it comes to dough</Title>
+      <Content />
+    </div>
+  );
+};
+
+export default Page;
